ec6229d78f1a68018ad188cef0df75067483c2fe
mavalero010 <maicolvpolo@gmail.com>
2024-04-19 14:52:02 -0500

    skills component finished

--- src/app/content/education/education.component.ts
+++ src/app/content/education/education.component.ts
@@ -3,10 +3,51 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-education',
   templateUrl: './education.component.html',
-  styleUrls: ['./education.component.css']
+  styleUrls: ['./education.component.css'],
 })
 export class EducationComponent {
   educationItems = [
-    {field:"egresado en ingeniería de sistemas",date:"agosto 2023 - diciembre 2023",info:"",time:"",url:"https://github.com/mavalero010"}
-  ]
+    {
+      field: 'egresado en ingeniería de sistemas',
+      date: 'enero 2016 - julio 2023',
+      info: 'universidad del norte',
+      time: '5 años de estudio',
+      url: '../../../assets/documents/diploma michael.pdf',
+    },
+    {
+      field: 'técnico en diseño gráfico',
+      date: 'enero 2015 - diciembre 2015',
+      info: 'ITSA (hoy UTB)',
+      time: '1 año de estudio',
+      url: '',
+    },
+    {
+      field: 'typescript avanzado',
+      date: 'octubre 2023',
+      info: 'linkedIn-Learning',
+      time: '2,45 horas de estudio',
+      url: 'https://www.linkedin.com/learning/certificates/a2c67a988c011d6dda9fe7d324a51532c7c3c33850c6082d88da8017f1dd0946',
+    },
+    {
+      field: 'node.js avanzado',
+      date: 'octubre 2023',
+      info: 'linkedIn-Learning',
+      time: '3 horas de estudio',
+      url: 'https://www.linkedin.com/learning/certificates/9d5ff449ad49441b747507d9a5927670468a75b735b751996b3451b6363fc42b',
+    },
+    {
+      field: 'describe cloud concepts',
+      date: 'septiembre 2023',
+      info: 'microsoft azure fundamentals',
+      time: '1 mes de estudio',
+      url: 'https://learn.microsoft.com/en-us/users/michaelandresvaleropolo-2167/achievements/ksjk2e6b',
+    },
+    {
+      field: 'python essentials 2',
+      date: 'septiembre 2023',
+      info: 'Cisco',
+      time: '2 semanas de estudio',
+      url: 'https://www.credly.com/badges/137470d5-10fd-494d-8ede-b74407f8f75d/linked_in_profile',
+    }
+  ];
 }
--- src/app/content/skills/skills.component.ts
+++ src/app/content/skills/skills.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class SkillsComponent {
 
+    languages = [
+      {name:'javascript', clname:'fa-brands fa-js bg-[#FFD43B] mb-r text-moon-primary'},
+      {name:'typescript', clname:'fa-brands fa-css3 bg-[#1a4fa3] mb-r text-moon-whitenew'},
+      {name:'java', clname:'fa-brands fa-java bg-[#d07206] mb-r text-moon-secondary'},
+      {name:'html', clname:'fa-brands fa-html5 bg-[#AA4C38] mr-3'},
+      {name:'python', clname:'fa-brands fa-python bg-[#07395f] mb-r text-moon-textlightselected'},
+      {name:'css', clname:'fa-brands fa-css3 bg-[#1a4fa3] mb-r text-moon-whitenew'},
+    ]
+    
+    frameworks = [
+      {name:'nodejs', clname:'fa-brands fa-js bg-[#FFD43B] mb-r text-moon-primary'},
+      {name:'springboot', clname:'fa-brands fa-java bg-[#d07206] mb-r text-moon-secondary'},
+      {name:'expressjs', clname:'fa-brands fa-js bg-moon-secondsuccess mb-r text-moon-whitenew'},
+      {name:'angular', clname:'fa-brands fa-angular bg-[#a32d14] mr-3'},
+    ]
+
+    tools = [
+      {name:'vs code', clname:'bg-[#1a4fa3] text-moon-whitenew'},
+      {name:'eclipse', clname:'bg-[#a60be3] border-moon-whitenew text-moon-whitenew'},
+      {name:'github', clname:'bg-[#07395f] border-moon-whitenew text-moon-whitenew'},
+      {name:'mongodb', clname:'bg-moon-secondsuccess mb-r text-moon-whitenew'},
+      {name:'postgreSQL', clname:'bg-[#1a4fa3] mb-r text-moon-whitenew'},
+      {name:'intellij idea', clname:'bg-[#FFD43B] bg-gradient-to-r from-moon-textlightselected to-[#a60be3]'},
+    ]
+  learning = [
+    {name:'c# [asp.net]'},
+    {name:'nest.js'},
+    {name:'aws s3'}
+  ]
 }
